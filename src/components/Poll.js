d17196022b50b0f9e7d7e3ebc3b8a8cf5f020dad
Logan Hart <2loganhart@gmail.com>
2018-07-25 14:22:03 -0700

    Redirect to home page on new poll submission, working on bug highlighting polls that user has already voted on

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { getPercentage } from '../utils/helpers';
-import { handleAddAnswer } from '../actions/answers';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getPercentage } from '../utils/helpers'
+import { handleAddAnswer } from '../actions/answers'
 
-const getVoteKeys = () => ['aVotes', 'bVotes', 'cVotes', 'dVotes'];
+const getVoteKeys = () => ['aVotes', 'bVotes', 'cVotes', 'dVotes']
 
 class Poll extends Component {
   handleAnswer = (answer) => {
@@ -15,7 +15,6 @@ class Poll extends Component {
       answer,
     }));
   }
-
   render() {
     if (this.props.poll === null) {
       return <p>This poll does not exist</p>
@@ -23,7 +22,7 @@ class Poll extends Component {
     const { poll, vote, authorAvatar } = this.props;
     const totalVotes = getVoteKeys()
       .reduce((total, key) => total + poll[key].length, 0);
-    
+
     return (
       <div className='poll-container'>
         <h1 className='question'>
@@ -35,16 +34,16 @@ class Poll extends Component {
         <ul>
           {['aText', 'bText', 'cText', 'dText'].map((key) => {
             const count = poll[key[0] + 'Votes'].length;
+
             return (
               <li
                 key={key}
                 onClick={() => {
                   if (vote === null && !this.answered) {
-                    this.handleAnswer(key[0]);
+                    this.handleAnswer(key[0])
                   }
                 }}
-                className={`option ${vote === key[0] ? 'chosen' : ''}`}
-              >
+                className={`option ${vote === key[0] ? 'chosen' : ''}`}>
                   {vote === null
                     ? poll[key]
                     : <div className='result'>
@@ -74,14 +73,14 @@ function mapStateToProps ({ authedUser, polls, users}, { match }) {
     }
     return poll[key].includes(authedUser)
       ? key
-      : vote;
+      : vote
   }, null);
   return {
     poll,
     vote,
     authedUser,
-    authorAvatar: users[poll.author].avatarURL,
-  };
+    authorAvatar: users[poll.author].avatarURL
+  }
 }
 
 export default connect(mapStateToProps)(Poll);
